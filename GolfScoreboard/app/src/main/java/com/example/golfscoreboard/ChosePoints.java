5dad06a7d600db5e8b92c65911be8276862c0663
Dressel <dressel.system@outlook.de>
2022-01-03 15:00:05 +0100

    Point Buttons implemented

--- GolfScoreboard/app/src/main/java/com/example/golfscoreboard/ChosePoints.java
+++ GolfScoreboard/app/src/main/java/com/example/golfscoreboard/ChosePoints.java
@@ -2,13 +2,107 @@ package com.example.golfscoreboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ChosePoints extends AppCompatActivity {
+public class ChosePoints extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chose_points);
+
+        btn1 = findViewById(R.id.point1);
+        btn1.setOnClickListener(this);
+
+        btn2 = findViewById(R.id.point2);
+        btn2.setOnClickListener(this);
+
+        btn3 = findViewById(R.id.point3);
+        btn3.setOnClickListener(this);
+
+        btn4 = findViewById(R.id.point4);
+        btn4.setOnClickListener(this);
+
+        btn5 = findViewById(R.id.point5);
+        btn5.setOnClickListener(this);
+
+        btn6 = findViewById(R.id.point6);
+        btn6.setOnClickListener(this);
+
+        btn7 = findViewById(R.id.point7);
+        btn7.setOnClickListener(this);
+
+        btn8 = findViewById(R.id.point8);
+        btn8.setOnClickListener(this);
+
+        btn9 = findViewById(R.id.point9);
+        btn9.setOnClickListener(this);
+
+        btn10 = findViewById(R.id.point10);
+        btn10.setOnClickListener(this);
+
+        btn11 = findViewById(R.id.point11);
+        btn11.setOnClickListener(this);
+
+        btn12 = findViewById(R.id.point12);
+        btn12.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.point1:
+                givePoints(1);
+                break;
+            case R.id.point2:
+                givePoints(2);
+                break;
+            case R.id.point3:
+                givePoints(3);
+                break;
+            case R.id.point4:
+                givePoints(4);
+                break;
+            case R.id.point5:
+                givePoints(5);
+                break;
+            case R.id.point6:
+                givePoints(6);
+                break;
+            case R.id.point7:
+                givePoints(7);
+                break;
+            case R.id.point8:
+                givePoints(8);
+                break;
+            case R.id.point9:
+                givePoints(9);
+                break;
+            case R.id.point10:
+                givePoints(10);
+                break;
+            case R.id.point11:
+                givePoints(11);
+                break;
+            case R.id.point12:
+                givePoints(12);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void givePoints(int points){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("points", Integer.toString(points));
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
 }
--- GolfScoreboard/app/src/main/java/com/example/golfscoreboard/MainActivity.java
+++ GolfScoreboard/app/src/main/java/com/example/golfscoreboard/MainActivity.java
@@ -9,50 +9,136 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn2;
-    Button btn3;
-    Button btn4;
-    String textresults;
+    Button btn2, btn3, btn4, btn6, btn7, btn8, btn10, btn11, btn12, btn14, btn15, btn16, btn18, btn19, btn20,
+            btn22, btn23, btn24, btn26, btn27, btn28, btn30, btn31, btn32, btn34, btn35, btn36, btn38, btn39, btn40;
     Button currentBtn;
 
-    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+
+    ActivityResultLauncher<Intent> startForPlayerName = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
         public void onActivityResult(ActivityResult result) {
             if(result != null && result.getResultCode() == RESULT_OK){
                 if(result.getData() != null){
-                    setNamePlayer(result.getData().getStringExtra("newName"), currentBtn);
+                    currentBtn.setText(result.getData().getStringExtra("newName"));
+                }
+            }
+        }
+    });
+
+    ActivityResultLauncher<Intent> startForPoints = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result != null && result.getResultCode() == RESULT_OK){
+                if(result.getData() != null){
+                    currentBtn.setText(result.getData().getStringExtra("points"));
 
                 }
             }
         }
     });
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn2 = (Button) findViewById(R.id.button2);
+        btn2 = findViewById(R.id.button2);
         btn2.setOnClickListener(this);
 
-        btn3 = (Button) findViewById(R.id.button3);
+        btn3 = findViewById(R.id.button3);
         btn3.setOnClickListener(this);
 
-        btn4 = (Button) findViewById(R.id.button4);
+        btn4 = findViewById(R.id.button4);
         btn4.setOnClickListener(this);
-    }
 
+        btn6 = findViewById(R.id.button6);
+        btn6.setOnClickListener(this);
 
+        btn7 = findViewById(R.id.button7);
+        btn7.setOnClickListener(this);
 
-    private void setNamePlayer(String newName, View v){
-        Button button = (Button) v;
-        button.setText(newName);
+        btn8 = findViewById(R.id.button8);
+        btn8.setOnClickListener(this);
+
+        btn10 = findViewById(R.id.button10);
+        btn10.setOnClickListener(this);
+
+        btn11 = findViewById(R.id.button11);
+        btn11.setOnClickListener(this);
+
+        btn12 = findViewById(R.id.button12);
+        btn12.setOnClickListener(this);
+
+        btn14 = findViewById(R.id.button14);
+        btn14.setOnClickListener(this);
+
+        btn15 = findViewById(R.id.button15);
+        btn15.setOnClickListener(this);
+
+        btn16 = findViewById(R.id.button16);
+        btn16.setOnClickListener(this);
+
+        btn18 = findViewById(R.id.button18);
+        btn18.setOnClickListener(this);
+
+        btn19 = findViewById(R.id.button19);
+        btn19.setOnClickListener(this);
+
+        btn20 = findViewById(R.id.button20);
+        btn20.setOnClickListener(this);
+
+        btn22 = findViewById(R.id.button22);
+        btn22.setOnClickListener(this);
+
+        btn23 = findViewById(R.id.button23);
+        btn23.setOnClickListener(this);
+
+        btn24 = findViewById(R.id.button24);
+        btn24.setOnClickListener(this);
+
+        btn26 = findViewById(R.id.button26);
+        btn26.setOnClickListener(this);
+
+        btn27 = findViewById(R.id.button27);
+        btn27.setOnClickListener(this);
+
+        btn28 = findViewById(R.id.button28);
+        btn28.setOnClickListener(this);
+
+        btn30 = findViewById(R.id.button30);
+        btn30.setOnClickListener(this);
+
+        btn31 = findViewById(R.id.button31);
+        btn31.setOnClickListener(this);
+
+        btn32 = findViewById(R.id.button32);
+        btn32.setOnClickListener(this);
+
+        btn34 = findViewById(R.id.button34);
+        btn34.setOnClickListener(this);
+
+        btn35 = findViewById(R.id.button35);
+        btn35.setOnClickListener(this);
+
+        btn36 = findViewById(R.id.button36);
+        btn36.setOnClickListener(this);
+
+        btn38 = findViewById(R.id.button38);
+        btn38.setOnClickListener(this);
+
+        btn39 = findViewById(R.id.button39);
+        btn39.setOnClickListener(this);
+
+        btn40 = findViewById(R.id.button40);
+        btn40.setOnClickListener(this);
     }
 
 
@@ -61,23 +147,141 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.button2:
                 currentBtn = btn2;
-                getResults();
+                getResultPlayerName();
                 break;
             case R.id.button3:
                 currentBtn = btn3;
-                getResults();
+                getResultPlayerName();
                 break;
             case R.id.button4:
                 currentBtn = btn4;
-                getResults();
+                getResultPlayerName();
                 break;
+            case R.id.button6:
+                currentBtn = btn6;
+                getResultHolePoints();
+                break;
+            case R.id.button7:
+                currentBtn = btn7;
+                getResultHolePoints();
+                break;
+            case R.id.button8:
+                currentBtn = btn8;
+                getResultHolePoints();
+                break;
+            case R.id.button10:
+                currentBtn = btn10;
+                getResultHolePoints();
+                break;
+            case R.id.button11:
+                currentBtn = btn11;
+                getResultHolePoints();
+                break;
+            case R.id.button12:
+                currentBtn = btn12;
+                getResultHolePoints();
+                break;
+            case R.id.button14:
+                currentBtn = btn14;
+                getResultHolePoints();
+                break;
+            case R.id.button15:
+                currentBtn = btn15;
+                getResultHolePoints();
+                break;
+            case R.id.button16:
+                currentBtn = btn16;
+                getResultHolePoints();
+                break;
+            case R.id.button18:
+                currentBtn = btn18;
+                getResultHolePoints();
+                break;
+            case R.id.button19:
+                currentBtn = btn19;
+                getResultHolePoints();
+                break;
+            case R.id.button20:
+                currentBtn = btn20;
+                getResultHolePoints();
+                break;
+            case R.id.button22:
+                currentBtn = btn22;
+                getResultHolePoints();
+                break;
+            case R.id.button23:
+                currentBtn = btn23;
+                getResultHolePoints();
+                break;
+            case R.id.button24:
+                currentBtn = btn24;
+                getResultHolePoints();
+                break;
+            case R.id.button26:
+                currentBtn = btn26;
+                getResultHolePoints();
+                break;
+            case R.id.button27:
+                currentBtn = btn27;
+                getResultHolePoints();
+                break;
+            case R.id.button28:
+                currentBtn = btn28;
+                getResultHolePoints();
+                break;
+            case R.id.button30:
+                currentBtn = btn30;
+                getResultHolePoints();
+                break;
+            case R.id.button31:
+                currentBtn = btn31;
+                getResultHolePoints();
+                break;
+            case R.id.button32:
+                currentBtn = btn32;
+                getResultHolePoints();
+                break;
+            case R.id.button34:
+                currentBtn = btn34;
+                getResultHolePoints();
+                break;
+            case R.id.button35:
+                currentBtn = btn35;
+                getResultHolePoints();
+                break;
+            case R.id.button36:
+                currentBtn = btn36;
+                getResultHolePoints();
+                break;
+            case R.id.button38:
+                currentBtn = btn38;
+                getResultHolePoints();
+                break;
+            case R.id.button39:
+                currentBtn = btn39;
+                getResultHolePoints();
+                break;
+            case R.id.button40:
+                currentBtn = btn40;
+                getResultHolePoints();
+                break;
+
+
+
             default:
                 break;
         }
     }
 
-    private void getResults(){
+    private void getResultPlayerName(){
         Intent intent = new Intent(this, ChangePlayerName.class);
-        startForResult.launch(intent);
+        startForPlayerName.launch(intent);
     }
+
+    private void getResultHolePoints(){
+        Intent intent = new Intent (this, ChosePoints.class);
+        startForPoints.launch(intent);
+
+    }
+
 }
