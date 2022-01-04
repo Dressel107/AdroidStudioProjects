96ef27ecff4aae881e3a42b2ff8d9d56e9988ffc
Dressel <dressel.system@outlook.de>
2022-01-05 00:01:39 +0100

    startactivity

--- GolfScoreboard/app/src/main/java/com/example/golfscoreboard/SplashActivity.java
+++ GolfScoreboard/app/src/main/java/com/example/golfscoreboard/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
-        final Intent i = new Intent(this, MainActivity.class);
+        final Intent i = new Intent(this, Start.class);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
---
+++ GolfScoreboard/app/src/main/java/com/example/golfscoreboard/Start.java
@@ -0,0 +1,39 @@
+package com.example.golfscoreboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Start extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        this.btn = findViewById(R.id.startspiel);
+        this.btn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText editText1 = findViewById(R.id.anzahlspieler);
+        EditText editText2 = findViewById(R.id.anzahllöcher);
+
+        String inputNuPlayer = editText1.getText().toString();
+        String inputNuHoles = editText2.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("anzahlSpieler", inputNuPlayer);
+        intent.putExtra("anzahlLöcher", inputNuHoles);
+
+        startActivity(intent);
+    }
+}
